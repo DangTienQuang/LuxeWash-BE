ae66f86f208470bf2f4d0a403b84f139a1c35c70
DangQuangTien <dangquangtien16102002@gmail.com>
2026-07-22 10:45:59 +0700

    feat: Integrate new Firebase service account key

--- API/Program.cs
+++ API/Program.cs
@@ -144,7 +144,7 @@ builder.Services.AddSingleton<PaddleOcrService>(sp =>
 QuestPDF.Settings.License = LicenseType.Community;
 
 // 5.3 Firebase Cloud Messaging Integration
-var firebaseKeyPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "smartwash-a9f29-firebase-adminsdk-fbsvc-8310300076.json");
+var firebaseKeyPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "smartwash-a9f29-firebase-adminsdk-fbsvc-a500ee26aa.json");
 if (File.Exists(firebaseKeyPath))
 {
     FirebaseAdmin.FirebaseApp.Create(new FirebaseAdmin.AppOptions()
